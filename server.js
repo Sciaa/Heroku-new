4c3a548920f045161e0eafc82389f4d1febea829
Harold Isaac Barrios <harold.barrios@outlook.com>
2017-07-07 17:19:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ app.post('/process_post', urlencodedParser, function (req, res) {
    res.end(JSON.stringify(response));
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-
-})
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
